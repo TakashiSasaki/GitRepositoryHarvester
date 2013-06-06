f5e59c8d1cd328a092882f8187203d98c7b0cbd9
Takashi SASAKI <takashi316@gmail.com>
2013-06-06 20:44:26 +0900

    pick up all .git directories

--- hello.py
+++ hello.py
@@ -1,10 +1,83 @@
-import os
-__author__ = 'sasaki'
+# -*- coding: utf-8 -*-
+
+import logging
+import os, atexit, json
+
+__author__ = 'Takashi SASAKI'
+globalState = None
+
+
+def initGlobalState():
+    global globalState
+    globalState = {
+        "foundGitRepositories": [],
+        "directoryStack": ["C:\\"],
+        "nVisitedDirectories": 0
+    }
+
+
+def saveGlobalState():
+    f = open("globalState", "w")
+    json.dump(globalState, f)
+    print("SAVE: " + json.dumps(globalState))
+
+
+def loadGlobalState():
+    try:
+        f = open("globalState", "r")
+    except IOError as e:
+        return
+    globalState = json.load(f)
+    print("LOAD: " + json.dumps(globalState))
+
+
+def popDirectory(directory_stack):
+    # print(directory_stack)
+    for root, dirs, files in os.walk(directory_stack[0]):
+        new_directories = [os.path.join(directory_stack[0], x) for x in dirs]
+        return directory_stack[0], dirs, files, new_directories + directory_stack[1:]
+    raise IOError("Can't get directories for %s" % directory_stack[0])
 
 
 def main():
-    print("abc")
-    pass
+    directory_stack = globalState["directoryStack"]
+    found_git_repositories = globalState["foundGitRepositories"]
+    n_visited_directories = globalState["nVisitedDirectories"]
+    count = 0
+    while len(directory_stack) > 0:
+        try:
+            root, dirs, files, directory_stack = popDirectory(directory_stack)
+        except IOError as e:
+            directory_stack = directory_stack[1:]
+            continue
+        n_visited_directories += 1
+        for d in dirs:
+            if d == ".git":
+                found_git_repositories.append(root + os.sep + d)
+        count += 1
+        if count >= 5000:
+            count = 0
+            globalState["directoryStack"] = directory_stack
+            globalState["foundGitRepositories"] = found_git_repositories
+            globalState["nVisitedDirectories"] = n_visited_directories
+            saveGlobalState()
+    saveGlobalState()
+
+
+def printResult():
+    global globalState
+    for x in globalState["foundGitRepositories"]:
+        print(x)
+
+
+def aborted1():
+    print("中断しちゃいましたよ")
+
 
 if __name__ == "__main__":
+    atexit.register(saveGlobalState)
+    initGlobalState()
+    loadGlobalState()
     main()
+    printResult()
+    saveGlobalState()
