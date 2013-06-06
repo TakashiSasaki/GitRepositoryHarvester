909806eeacef557dd80f000bcf76a535da8876ec
Takashi SASAKI <takashi316@gmail.com>
2013-06-06 21:26:20 +0900

    ignore some system folders

--- hello.py
+++ hello.py
@@ -11,6 +11,8 @@ def initGlobalState():
     global globalState
     globalState = {
         "foundGitRepositories": [],
+        "foundBzrRepositories": [],
+        "foundSvnRepositories": [],
         "directoryStack": ["C:\\"],
         "nVisitedDirectories": 0
     }
@@ -19,7 +21,7 @@ def initGlobalState():
 def saveGlobalState():
     global globalState
     f = open("globalState", "w")
-    json.dump(globalState, f)
+    json.dump(globalState, f, ensure_ascii=False)
     print("SAVE: " + json.dumps(globalState))
 
 
@@ -29,34 +31,57 @@ def loadGlobalState():
         f = open("globalState", "r")
     except IOError as e:
         return
-    globalState = json.load(f)
+    globalState.update(json.load(f))
     print("LOAD: " + json.dumps(globalState))
 
-def popDirectory(directory_stack):
+
+def popDirectory(directory_path):
     # print(directory_stack)
-    for root, dirs, files in os.walk(directory_stack[0]):
+    for root, dirs, files in os.walk(directory_path):
         new_directories = []
         for d in dirs:
             if d == "$Recycled.Bin": continue
-            new_directories.append(os.path.join(directory_stack[0], d))
-        return directory_stack[0], dirs, files, new_directories
-    raise IOError("Can't get directories for %s" % directory_stack[0])
+            new_directories.append(os.path.join(directory_path, d))
+        return directory_path, dirs, files, new_directories
+    raise IOError("Can't get directories for %s" % directory_path)
+
 
 def main():
     global globalState
     directory_stack = globalState["directoryStack"]
     found_git_repositories = globalState["foundGitRepositories"]
+    found_svn_repositories = globalState["foundSvnRepositories"]
+    found_bzr_repositories = globalState["foundBzrRepositories"]
     n_visited_directories = globalState["nVisitedDirectories"]
     count = 0
     while len(directory_stack) > 0:
+        if directory_stack[0] == "C:\\Windows":
+            directory_stack = directory_stack[1:]
+            continue
+        if directory_stack[0] == "C:\\WindowsESD":
+            directory_stack = directory_stack[1:]
+            continue
+        if directory_stack[0] == "C:\\Program Files":
+            directory_stack = directory_stack[1:]
+            continue
+        if directory_stack[0] == "C:\\Program Files (x86)":
+            directory_stack = directory_stack[1:]
+            continue
+
         try:
-            root, dirs, files, new_directories = popDirectory(directory_stack)
+            root, dirs, files, new_directories = popDirectory(directory_stack[0])
         except IOError as e:
             directory_stack = directory_stack[1:]
             continue
         n_visited_directories += 1
         if ".git" in dirs:
             found_git_repositories.append(root)
+            directory_stack = directory_stack[1:]
+        if ".svn" in dirs:
+            found_svn_repositories.append(root)
+            directory_stack = directory_stack[1:]
+        if ".bzr" in dirs:
+            found_bzr_repositories.append(root)
             directory_stack = directory_stack[1:]
         else:
             directory_stack = new_directories + directory_stack[1:]
@@ -65,6 +90,8 @@ def main():
             count = 0
             globalState["directoryStack"] = directory_stack
             globalState["foundGitRepositories"] = found_git_repositories
+            globalState["foundBzrRepositories"] = found_bzr_repositories
+            globalState["foundSvnRepositories"] = found_svn_repositories
             globalState["nVisitedDirectories"] = n_visited_directories
             saveGlobalState()
     saveGlobalState()
@@ -75,6 +102,7 @@ def printResult():
     global globalState
     for x in globalState["foundGitRepositories"]:
         f.write(x + "\n")
+
 
 def aborted1():
     print("中断しちゃいましたよ")
